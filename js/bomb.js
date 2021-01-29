a6020e357db82d7d46d0e9701f871ee23837fefa
AlrightErin27 <erinmontybruce@gmail.com>
2021-01-29 13:01:39 -0800

    added more styling and frame of JS

--- js/bomb.js
+++ js/bomb.js
@@ -1,10 +1,29 @@
 /*----- constants -----*/
+const INITIAL_TIME = 30
 
 /*----- app's state (variables) -----*/
+//var to store timer's countdown
+let timeRemaining = 0
+//store our timer's set interval so we can clear it
+let countdown = null
+// boolean for game over
+let gameOver = false
+//keep track of clicked wires
+let wireState = {
+    blue: false,
+    green: false,
+    red: false,
+    yellow: false,
+    white: false
+}
 
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
+// event listeners on the wires
+//
+// event listeners on the timer- using set interval & stop:
+// 
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('loaded!');
