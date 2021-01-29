2d1e7af3bf36089037a09b8c465f3a43f9c3c3fd
AlrightErin27 <erinmontybruce@gmail.com>
2021-01-29 13:54:48 -0800

    boiler plate JS added

--- js/bomb.js
+++ js/bomb.js
@@ -16,15 +16,53 @@ let wireState = {
     yellow: false,
     white: false
 }
+//array of wires that need to be clicked to win the game
+let wiresToCut = []
 
 /*----- cached element references -----*/
+//event listeners on wiress
+let wireBoxEl = null
+//each wire element
+let wires = []
+//background image
+let backgroundEl = null
+//timer 
+let timerEl = null
+function updateClock() {
+    console.log("Countdown the timer!")
+}
+//run to set up game
+function initializeGame() {
+        console.log("Set the game up!")
+}
+//reset button
+let resetButtonEl = null
 
+//handles reset button click
+function resetGame() {
+    console.log("Reset game!")
+}
+//handles reset button click
+function cutWire(event) {
+    console.log("cut a wire!" , event);
+}
+//handle game over state
+function endGame(isGameWon) {
+    console.log("END GAME")
+}
 /*----- event listeners -----*/
 // event listeners on the wires
-//
-// event listeners on the timer- using set interval & stop:
-// 
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     console.log('loaded!');
+//BACKGROUND IMAGE manippulator
+    backgroundEl = document.querySelector("main")
+//countdown timer
+    timerEl = document.querySelector("#timer")
+//RESET BUTTON
+    resetButtonEl = document.querySelector("#reset")
+    resetButtonEl.addEventListener("click" , resetGame)
+//WIREBOX & click event listener
+    wireBoxEl = document.querySelector("#wireBox")
+    wireBoxEl.addEventListener("click" , cutWire)
+    wires = wireBoxEl.children
 })
